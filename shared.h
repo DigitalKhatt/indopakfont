5b11b913e4605664d1f572e82ef8fe36379b2b2e
Amine Anane <ananeamine@gmail.com>
2025-05-14 10:52:50 -0400

    Remove stdcall

--- shared.h
+++ shared.h
@@ -2,9 +2,9 @@
 
 #include "indopak.h"
 
-extern "C" __declspec(dllexport) Automedina* __stdcall font_create(OtLayout* layout, Font* font, bool extended);
+extern "C" __declspec(dllexport) Automedina* font_create(OtLayout* layout, Font* font, bool extended);
 
-extern "C" void __stdcall font_delete(indopak::IndoPak* p_obj) {
+extern "C" void font_delete(indopak::IndoPak* p_obj) {
 	try {
 		delete p_obj;
 	}
