0f80afa818d78ff3524d405bffca49665be01ec5
Amine Anane <ananeamine@gmail.com>
2026-06-10 20:31:01 -0400

    Change to edges

--- indopak.cpp
+++ indopak.cpp
@@ -17,11 +17,11 @@ using namespace std;
 namespace indopak {
 
 void IndoPak::generateGlyphs() {
-  mp_edge_object* edges = font->getEdges();
+  auto edgess = font->getEdges();
 
   glyphs.clear();
 
-  while (edges) {
+  for (auto edges : edgess) {
     auto name = QString(edges->charname);
 
     if (name != "alternatechar") {
@@ -68,8 +68,6 @@ void IndoPak::generateGlyphs() {
         }
       }
     }
-
-    edges = edges->next;
   }
 
   auto addFake = [this](QString glyphName, quint16 unicode, quint16 codechar) {
