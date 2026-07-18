9e7679ecd2a8ce37112050b5aff8fa5906331ab0
Amine Anane <ananeamine@gmail.com>
2026-07-18 14:14:09 -0400

    Use portable font plugin exports

--- shared.h
+++ shared.h
@@ -1,8 +1,9 @@
 #pragma once
 
+#include "fontplugin.h"
 #include "indopak.h"
 
-extern "C" __declspec(dllexport) Automedina* font_create(OtLayout* layout, MPFont* font, bool extended);
+extern "C" DIGITALKHATT_FONT_EXPORT Automedina* font_create(OtLayout* layout, MPFont* font, bool extended);
 
 extern "C" void font_delete(indopak::IndoPak* p_obj) {
 	try {
